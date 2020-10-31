752dfb0c5517d23c1e05fab0277dc0e1203989c2
prakharagarwal1 <prakharagarwal1990@gmail.com>
2020-10-31 19:40:34 +0530

    adding title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect} from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { Link } from "react-router-dom";
 import Pokeball from "../pokeball.png";
 import { connect } from "react-redux";
@@ -14,8 +14,8 @@ const Home = ({ posts }) => {
   //     console.log("This is a return statement in UseEffect ---> Home.js Component")  
   //   };
   // }, []);
-  
-  
+
+
   // useEffect(() => {
   //    
   //   console.log("This is a  2 main statement in UseEffect ---> Home.js Component")    
@@ -26,59 +26,59 @@ const Home = ({ posts }) => {
   // },[]);
 
 
-//   useEffect(() => {
-//     console.log("USE EFFECT FUNCTION TRIGGERED");
-    
-//     setTimeout(() => {
-//        console.log("SET TIME OUT CALLED AFTER 3 SECONDS");
-//     }, 1000)
-// });
-// useLayoutEffect(() => {
-//     console.log("USE LAYOUT EFFECT FUNCTION TRIGGERED");
-// });
+  //   useEffect(() => {
+  //     console.log("USE EFFECT FUNCTION TRIGGERED");
 
-   
-// const cities = ["Kampala", "Nairobi", "Lagos"];
-//    console.log(...cities);
-//    console.log(cities);
-  
-// function someFn() {   
-//   if (true) {
-//         //defined locally
-//     //Its scope ends where curly braces ends 
-//     var localVar = 1000;
-//     console.log(localVar)      //ok
-//   } 
-//   console.log(localVar)      //ok
-//    function nested() {         
-//     console.log(localVar)      //ok
-//   } 
-// } 
-// //console.log(localVar) //Error
-  
-  
+  //     setTimeout(() => {
+  //        console.log("SET TIME OUT CALLED AFTER 3 SECONDS");
+  //     }, 1000)
+  // });
+  // useLayoutEffect(() => {
+  //     console.log("USE LAYOUT EFFECT FUNCTION TRIGGERED");
+  // });
+
+
+  // const cities = ["Kampala", "Nairobi", "Lagos"];
+  //    console.log(...cities);
+  //    console.log(cities);
+
+  // function someFn() {   
+  //   if (true) {
+  //         //defined locally
+  //     //Its scope ends where curly braces ends 
+  //     var localVar = 1000;
+  //     console.log(localVar)      //ok
+  //   } 
+  //   console.log(localVar)      //ok
+  //    function nested() {         
+  //     console.log(localVar)      //ok
+  //   } 
+  // } 
+  // //console.log(localVar) //Error
+
+
   const postList = posts.length ? (
     posts.map((post) => {
       return (
         <div className="post card" key={post.id}>
-       <img src={Pokeball} alt="No Image Found" />
-       <div className="card-content">
-         <Link to={"/" + post.id}>
-           <span className="card-title red-text">{post.title}</span>
-         </Link>
-         <p>{post.body}</p>
-       </div>
-     </div>
-   );
- })
+          <img src={Pokeball} alt="No Image Found" />
+          <div className="card-content">
+            <Link to={"/" + post.id}>
+              <span className="card-title red-text">{post.title}</span>
+            </Link>
+            <p>{post.body}</p>
+          </div>
+        </div>
+      );
+    })
   ) : (
-    <div className="center">No Posts to Display</div>
-  );
+      <div className="center">No Posts to Display</div>
+    );
 
   return (
     <React.Fragment>
       <div className="container home">
-        <h4 className="center">Home </h4>
+        <h4 className="center">Application Home </h4>
         {postList}
       </div>
     </React.Fragment>
